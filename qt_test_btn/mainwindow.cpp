b67d114dd26f5b50a1d7ff81370070819f31a710
PierreViland <132554462+PierreViland@users.noreply.github.com>
2023-07-10 08:03:08 +0200

    Ajout projet via Qt designer

---
+++ qt_test_btn/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+/**
+ * @brief MainWindow::MainWindow Constructeur de la classe
+ * @param parent
+ */
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->monCompteur = 0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btnMoins_clicked()
+{
+    this->monCompteur--;
+    ui->lblAffichage->setText(QString::number(this->monCompteur));
+    ui->lblAffichage->adjustSize();
+}
+
+
+void MainWindow::on_btnPlus_clicked()
+{
+    this->monCompteur++;
+    ui->lblAffichage->setText(QString::number(this->monCompteur));
+    ui->lblAffichage->adjustSize();
+
+}
+
+
+
+
